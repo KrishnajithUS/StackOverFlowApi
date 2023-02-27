6f759a273a18aa1c6fe774107119d8905eecaafd
Krishnajith <krishnajithus673@gmail.com>
2023-02-27 18:09:11 +0530

    completed

--- StackOverFlowApi/backend/backend/settings.py
+++ StackOverFlowApi/backend/backend/settings.py
@@ -106,7 +106,7 @@ CACHES = {
 }
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
-CACHE_TTL = 60 * 10
+CACHE_TTL = 60 * 20
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
